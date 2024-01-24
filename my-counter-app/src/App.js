716e97f66194d3672396dc3aa99f16e280bbdd6c
Patrick Annan <you@example.compatrickannan@hotmail.com>
2024-01-24 14:24:20 -0500

    styled counter with bg color, h1 color, pic of count, and button colors

--- my-counter-app/src/App.js
+++ my-counter-app/src/App.js
@@ -1,14 +1,26 @@
-import {React, useState } from 'react'
+import { React, useState } from 'react'
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="counter">
-      <h1>React Counter</h1>
-      <span className="counter_output"></span>
-      <div className='btn_container'></div>
-        <button className='control_btn'>+</button>
-        <button className='control_btn'>-</button>
-        <button className='reset'>Reset</button>
+      <h1 style={{ backgroundColor:'cornsilk', color: 'blue', textAlign: 'center' }}>React Counter</h1>
+      
+      <div style={{ textAlign: 'center', backgroundColor: 'cornsilk', padding: '3em' }}>
+      
+        <h2 className>{count}</h2>
+          <span className="counter_output"></span>
+            <div className='btn_container'>
+              <button style={{ color: 'green' }} className='control_btn add' onClick={() => setCount(count + 1)}>+</button>
+              
+              <button style={{ color: 'red' }} className='control_btn minus' onClick={() => setCount(count - 1)}>-</button>
+              
+              <button className='reset' onClick={() => setCount(0)}>Reset</button>
+              <br style={{ padding: '1.5em'}}></br>
+              <img src='TheCount.jpg' alt="I'm counting!"></img>
+            </div>
+      </div>
     </div>
   )
-};
+}; 
